6179d80d2d60a350c69bfeeaaf1fa63832243088
zen <zenon.deviagge@gmail.com>
2024-04-04 00:18:14 -0300

    Display links text imported from data.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,16 +4,30 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(true)
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className='logo' alt='logo' />
-          <button className='nav-toggle'>
-            <FaBars/>
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
           </button>
         </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )
