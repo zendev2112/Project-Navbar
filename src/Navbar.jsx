063420a8f0fda2e94520863941800a3082c6b048
zen <zenon.deviagge@gmail.com>
2024-04-06 18:49:07 -0300

    add static transition to links container

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,29 +5,33 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(true)
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div className={showLinks ? "links-container show-container" : 'links-container'}>
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   )
